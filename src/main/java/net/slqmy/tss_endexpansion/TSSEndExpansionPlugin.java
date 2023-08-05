8d31b435c7d3eb98188f2100959b6cf0d44b9027
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-08-05 23:47:03 +0100

    📁 Initial commit.

---
+++ src/main/java/net/slqmy/tss_endexpansion/TSSEndExpansionPlugin.java
@@ -0,0 +1,17 @@
+package net.slqmy.tss_endexpansion;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TSSEndExpansionPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
