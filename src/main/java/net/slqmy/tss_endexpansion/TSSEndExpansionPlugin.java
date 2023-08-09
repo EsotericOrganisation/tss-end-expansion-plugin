ff9ef44ff52d2e08afc48201f9c4672632ba58e3
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-08-09 17:13:49 +0100

    🐉 Registered end portal listener.

--- src/main/java/net/slqmy/tss_endexpansion/TSSEndExpansionPlugin.java
+++ src/main/java/net/slqmy/tss_endexpansion/TSSEndExpansionPlugin.java
@@ -1,17 +1,13 @@
 package net.slqmy.tss_endexpansion;
 
+import net.slqmy.tss_endexpansion.event.EndPortalListener;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TSSEndExpansionPlugin extends JavaPlugin {
 
-    @Override
-    public void onEnable() {
-        // Plugin startup logic
-
-    }
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
+	@Override
+	public void onEnable() {
+		Bukkit.getPluginManager().registerEvents(new EndPortalListener(this), this);
+	}
 }
